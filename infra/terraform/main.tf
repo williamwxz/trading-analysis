bd4b458835effd4034c3ae3812669fc4bb07317e
William <williamwxz@gmail.com>
2026-04-23 18:21:32 -0500

    feat: right-size always-on ECS task to 0.5vCPU/1GB

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -390,8 +390,8 @@ resource "aws_ecs_task_definition" "dagster" {
   family                   = "${local.name_prefix}-dagster"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 2048
-  memory                   = 8192
+  cpu                      = 512
+  memory                   = 1024
   execution_role_arn       = aws_iam_role.ecs_execution.arn
   task_role_arn            = aws_iam_role.ecs_task.arn
 
