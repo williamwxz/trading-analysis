37045441503ef1361d3e3ede8b2faf7b7cce8c3a
William <williamwxz@gmail.com>
2026-05-19 21:17:10 -0700

    scale up prod pnl consumer

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1237,7 +1237,7 @@ locals {
       enable_real_trade = "false"
       enable_bt         = "false"
       group_id          = "pnl-consumer-prod-2"
-      desired_count     = 0
+      desired_count     = 1
     }
     real-trade = {
       enable_price      = "false"
